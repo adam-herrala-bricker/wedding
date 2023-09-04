671cc2616fce36907c2b00d44a3272afb687f31d
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-09-04 18:47:55 +0300

    prod DB connection

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -12,10 +12,25 @@ const ADMIN_KEY = process.env.ADMIN_KEY
 
 const NODE_ENV = process.env.NODE_ENV
 
+const dbFinder = () => {
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            return process.env.DEV_MONGODB_URI
+        case 'production':
+            return process.env.PROD_MONGODB_URI
+        case 'testing':
+            return process.env.TEST_MONGODB_URI
+    }
+}
+
+const mongourl = dbFinder()
+
+
+/*
 const mongourl = NODE_ENV === 'development'
     ? process.env.DEV_MONGODB_URI
     : 'only dev DB set'
-
+*/
 
 
 module.exports = {mongourl, ADMIN_KEY, PORT, TOKEN, RANDY_HASH, NODE_ENV, SECRET, SECRET2}
