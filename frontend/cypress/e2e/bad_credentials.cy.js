f6a7f82fe5ad3bd7d7925259544cf5883ff55717
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-12-07 09:47:23 +0200

    bad login tests

--- frontend/cypress/e2e/bad_credentials.cy.js
+++ frontend/cypress/e2e/bad_credentials.cy.js
@@ -1,11 +1,43 @@
 import text from '../../src/resources/dictionary';
+import {configDB, enterSite} from '../utils/functions';
+import {adminUserInfo, baseURL} from '../utils/constants';
 
-it('Bad entry key', () => {
-  cy.visit('http://localhost:3003');
+describe('Bad credentials', () => {
+  beforeEach(() => {
+    configDB();
+    cy.visit(baseURL);
+  });
 
-  cy.get('input[name="entry"]').type('wrong_entry_key');
-  cy.get('button').click();
+  it('Bad entry key', () => {
+    cy.get('input[name="entry"]').type('wrong_entry_key');
+    cy.get('button').click();
 
-  cy.contains(text['entryError'].suo);
-  cy.contains(text['entryError'].eng);
+    cy.contains(text['entryError'].suo);
+    cy.contains(text['entryError'].eng);
+  });
+
+  it('Bad user login (suo)', () => {
+    enterSite();
+
+    // try to log in with wrong password
+    cy.get('button[name="show-login"]').click();
+    cy.get('input[name="Username"]').type(adminUserInfo.username);
+    cy.get('input[name="Password"]').type('wrong_admin_password');
+    cy.get('button[id="login-button"]').click();
+
+    // renders correct error message
+    cy.contains(text['loginError'].suo);
+  });
+
+  it.only('Bad user login (eng)', () => {
+    enterSite();
+    cy.get('button[name="eng-flag"]').click(); // sets to English
+
+    cy.get('button[name="show-login"]').click();
+    cy.get('input[name="Username"]').type(adminUserInfo.username);
+    cy.get('input[name="Password"]').type('wrong_admin_password');
+    cy.get('button[id="login-button"]').click();
+
+    cy.contains(text['loginError'].eng);
+  });
 });
