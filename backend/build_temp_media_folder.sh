00d130c94dfbdc5fcd4a083ef6977cd20650eccf
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-12-13 10:14:52 +0200

    setup for demo admin tests

--- backend/build_temp_media_folder.sh
+++ backend/build_temp_media_folder.sh
@@ -7,9 +7,7 @@ mkdir media/images/web-res &&
 mkdir media/audio &&
 
 # in production env, every web res image is already there
-cp media_testing/images/_DSC0815.jpg media/images/web-res &&
-cp media_testing/images/_DSC2591.jpg media/images/web-res &&
-cp media_testing/images/_DSC2596.jpg media/images/web-res &&
+cp media_testing/images/*.jpg media/images/web-res &&
 
 cp media_testing/audio/down-the-aisle.mp3 media/audio &&
 
---
+++ backend/tests/demo_admin.test.js
@@ -0,0 +1,7 @@
+
+
+// demo admin requests fail without demo admin token
+// demo admin token authenticates demo admi requests
+// demo admin token does NOT autheticate default admin requests
+
+// same files can't be added to default and demo DBs
