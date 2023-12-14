51181d0377a01162f7328c85d6eec7c06ad7c43a
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-12-14 11:06:47 +0200

    initial mobile framework

---
+++ mobile/src/components/Entry.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+
+const Entry = () => {
+    return (
+        <View>
+            <Text>
+                'Entry page!'
+            </Text>
+        </View>
+    )
+}
+
+export default Entry
---
+++ mobile/src/components/GridView.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+
+const GridView = () => {
+    return (
+        <View>
+            <Text>
+                'Grid view!'
+            </Text>
+        </View>
+    )
+}
+
+export default GridView
